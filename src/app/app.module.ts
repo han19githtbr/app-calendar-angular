3ba8e635c7134129130833fbef907c0781b2a295
han19githtbr <milliance23@gmail.com>
2023-06-14 23:14:52 -0300

    teste finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { ComponentesModule } from './components/component.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { SafePipeModule } from 'safe-pipe';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule, SafePipeModule, MatSnackBarModule,
+    RouterModule.forRoot([]),
+    ComponentesModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    routing,
+    BrowserAnimationsModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent {
+  calendarDates: { date: Date, appointments: { title: string, time: string }[] }[] = [];
+  newAppointment: { title: string, date: Date, time: string } = { title: '', date: null as any, time: '' };
+
+  addAppointment() {
+    if (this.newAppointment.title && this.newAppointment.date && this.newAppointment.time) {
+      const existingDate = this.calendarDates.find(dateObj =>
+        dateObj.date.toDateString() === this.newAppointment.date.toDateString()
+      );
+
+      if (existingDate) {
+        existingDate.appointments.push({ title: this.newAppointment.title, time: this.newAppointment.time });
+      } else {
+        this.calendarDates.push({
+          date: this.newAppointment.date,
+          appointments: [{ title: this.newAppointment.title, time: this.newAppointment.time }]
+        });
+      }
+
+      this.newAppointment = { title: '', date: null as any, time: '' };
+    }
+  }
+
+  deleteAppointment(dateObj: { date: Date, appointments: { title: string, time: string }[] }, appointment: { title: string, time: string }) {
+    const index = dateObj.appointments.indexOf(appointment);
+    if (index !== -1) {
+      dateObj.appointments.splice(index, 1);
+    }
+  }
+
+  onDrop(event: CdkDragDrop<{ date: Date, appointments: { title: string, time: string }[] }[]>) {
+    moveItemInArray(this.calendarDates, event.previousIndex, event.currentIndex);
+  }
+
+}
